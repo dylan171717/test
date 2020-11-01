1d0833fdcd179729e4e2011b3e8be01121e172db
pordylangamerx24@gmail.com <prodylangamerx24@gmail.com>
2020-11-01 12:43:17 -0500

    comment

--- sketch.js
+++ sketch.js
@@ -41,8 +41,8 @@ function setup(){
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
+    console.log("ok");
+    console.log("umm");
     console.log(box2.body.angle);
     box1.display();
     box2.display();
